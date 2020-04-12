8b1956ec931f9e382bd5421e14da88c644fd873f
Matthew Mullin <mattymullin@Matthews-MBP-2.attlocal.net>
2020-04-12 08:39:39 -0400

    changed creating elemnts to updating HTML so flexbox would opperate correctly.

--- script.js
+++ script.js
@@ -21,23 +21,6 @@ let values = ['As', 'King', 'Queen', 'Jack',
 
 // Establish elements on page
 
-const gameArea = document.createElement('canvas')
 
-const hit = document.createElement('button')
-hit.innerText = "Hit"
-document.body.appendChild(hit)
+// Board interactions
 
-const stand = document.createElement('button')
-stand.innerText = "Stand"
-document.body.appendChild(stand)
-
-const bet = document.createElement('input')
-bet.classList = "board"
-bet.setAttribute('type', 'form')
-bet.setAttribute('placeholder', 'bet amount')
-document.body.appendChild(bet)
-
-const betSubmit = document.createElement('input')
-betSubmit.classList = "board"
-betSubmit.setAttribute('type', 'submit')
-document.body.appendChild(betSubmit)
